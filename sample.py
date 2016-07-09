06791b4023386521bc0b57364f3d3cc0b57cc3a5
llby <llby@shingor.net>
2016-07-09 20:34:29 +0900

    rename package

--- sample.py
+++ sample.py
@@ -1,6 +1,6 @@
 import os
-import grep_result
-import grep_result.grep as grep
+import grep-result
+import grep-result.grep as grep
 
 
 def print_all(record):
@@ -11,12 +11,12 @@ home = os.path.abspath(os.path.dirname(__file__))
 path = os.path.abspath(os.path.dirname(__file__))
 keyword = "def \\w"
 
-grep_result.init(home)
+grep-result.init(home)
 
-grep_result.listing(grep.grep, path, keyword)
-grep_result.parse(grep.parse)
-grep_result.output(print_all)
+grep-result.listing(grep.grep, path, keyword)
+grep-result.parse(grep.parse)
+grep-result.output(print_all)
 
-grep_result.listing(grep.git_grep, path, keyword)
-grep_result.parse(grep.parse)
-grep_result.output(print_all)
+grep-result.listing(grep.git_grep, path, keyword)
+grep-result.parse(grep.parse)
+grep-result.output(print_all)
---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+from setuptools import find_packages
+
+setup(
+    name = "grep-result",
+    version = "0.0.1",
+    packages=find_packages(),
+    author="llby",
+    author_email="llby@shingor.net",
+    license="MIT",
+    classifiers=["Topic :: Utilities"]
+)
